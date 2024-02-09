e0965e9759aaaa08b0f9ff119e75a68b4660daff
Isiah Fletcher <Isiahfletcher7@gmail.com>
2024-02-09 12:12:08 -0700

    Init commit

---
+++ src/consts.ts
@@ -0,0 +1,12 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Isiah Fletcher";
+export const SITE_DESCRIPTION =
+  "Isiah is a musician, writer, and software engineer based in Boise, Idaho.";
+export const SOCIAL_LINKS = {
+  facebook: "https://www.facebook.com/fletchmydrumsticks/",
+  instagram: "https://www.instagram.com/fletchmydrumsticks/",
+  youtube: "https://www.youtube.com/channel/UCeV7RrWm2WOt6gMXxXGdkUw",
+  github: "https://github.com/Ifletcher668",
+};
