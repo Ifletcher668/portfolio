efc8b3e283974bce21ab5a46dcbd1f851733ca62
Isiah Fletcher <Isiahfletcher7@gmail.com>
2024-02-15 09:24:08 -0700

    Create new pages

--- src/consts.ts
+++ src/consts.ts
@@ -1,12 +1,31 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = "Isiah Fletcher";
 export const SITE_DESCRIPTION =
   "Isiah is a musician, writer, and software engineer based in Boise, Idaho.";
+
 export const SOCIAL_LINKS = {
   facebook: "https://www.facebook.com/fletchmydrumsticks/",
   instagram: "https://www.instagram.com/fletchmydrumsticks/",
   youtube: "https://www.youtube.com/channel/UCeV7RrWm2WOt6gMXxXGdkUw",
   github: "https://github.com/Ifletcher668",
+};
+
+export const BAND_LINKS = [
+  { title: "Aterrima", href: "/music/aterrima" },
+  { title: "Weald and Woe", href: "/music/weald-and-woe" },
+];
+
+export const NAV_LINKS = [
+  {
+    title: "Music",
+    href: "/music",
+    subLinks: BAND_LINKS,
+  },
+  {
+    title: "Work",
+    href: "/work",
+  },
+];
+
+export const EXTERNAL_LINKS = {
+  shelbzWebsite: "https://shelbzcitrine.com",
 };
