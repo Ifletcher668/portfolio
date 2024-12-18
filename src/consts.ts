dd8f4bb30997147c8c09e3327018f972296992ea
Isiah Fletcher <Isiahfletcher7@gmail.com>
2024-12-17 16:56:21 -0800

    Rennovation

--- src/consts.ts
+++ src/consts.ts
@@ -1,32 +1,37 @@
 export const SITE_TITLE = "Isiah Fletcher";
 export const SITE_DESCRIPTION =
-  "Isiah is a musician, writer, and software engineer based in Boise, Idaho.";
+  "Isiah is a musician, writer, and software engineer based in Salem, Oregon.";
 
 export const SOCIAL_LINKS = {
   facebook: "https://www.facebook.com/fletchmydrumsticks/",
-  instagram: "https://www.instagram.com/fletchmydrumsticks/",
   youtube: "https://www.youtube.com/channel/UCeV7RrWm2WOt6gMXxXGdkUw",
   github: "https://github.com/Ifletcher668",
 };
 
 export const BAND_LINKS = {
   aterrima: { title: "Aterrima", href: "/music/aterrima" },
-  wealdAndWoe: { title: "Weald and Woe", href: "/music/weald-and-woe" },
+  wealdAndWoe: { title: "Weald & Woe", href: "/music/weald-and-woe" },
 };
 
 export const BAND_LINKS_ARRAY = Object.values(BAND_LINKS);
 
-export const NAV_LINKS = [
-  {
+export const PAGE_LINKS = {
+  music: {
     title: "Music",
     href: "/music",
     subLinks: BAND_LINKS_ARRAY,
   },
-  {
+  work: {
     title: "Work",
     href: "/work",
   },
-];
+  hobbies: {
+    title: "Interests",
+    href: "/hobbies",
+  },
+};
+
+export const NAV_LINKS_ARRAY = Object.values(PAGE_LINKS);
 
 export const EXTERNAL_LINKS = {
   shelbzWebsite: "https://shelbzcitrine.com",
