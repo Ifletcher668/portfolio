e5aca1e12f4dd45522c18522dab73403640e46a3
Isiah Fletcher <Isiahfletcher7@gmail.com>
2025-01-19 20:47:37 -0800

    Add project pages and links. General style updates

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,10 @@ export const SITE_TITLE = "Isiah Fletcher";
 export const SITE_DESCRIPTION =
   "Isiah is a musician, writer, and software engineer based in Salem, Oregon.";
 
+export const EXTERNAL_LINKS = {
+  shelbzWebsite: "https://shelbzcitrine.com",
+};
+
 export const SOCIAL_LINKS = {
   facebook: "https://www.facebook.com/fletchmydrumsticks/",
   youtube: "https://www.youtube.com/channel/UCeV7RrWm2WOt6gMXxXGdkUw",
@@ -13,6 +17,18 @@ export const BAND_LINKS = {
   wealdAndWoe: { title: "Weald & Woe", href: "/music/weald-and-woe" },
 };
 
+export const PROJECT_LINKS = {
+  threeJsJourney: {
+    title: "Three.js Journey",
+    href: "/projects/three-js-journey",
+  },
+  shelbzWebsite: {
+    title: "Portfolio for Shelbz Citrine",
+    href: EXTERNAL_LINKS.shelbzWebsite,
+  },
+};
+
+export const PROJECT_LINKS_ARRAY = Object.values(PROJECT_LINKS);
 export const BAND_LINKS_ARRAY = Object.values(BAND_LINKS);
 
 export const PAGE_LINKS = {
@@ -29,10 +45,11 @@ export const PAGE_LINKS = {
     title: "Interests",
     href: "/hobbies",
   },
+  projects: {
+    title: "Projects",
+    href: "/projects",
+    subLinks: PROJECT_LINKS_ARRAY,
+  },
 };
 
 export const NAV_LINKS_ARRAY = Object.values(PAGE_LINKS);
-
-export const EXTERNAL_LINKS = {
-  shelbzWebsite: "https://shelbzcitrine.com",
-};
