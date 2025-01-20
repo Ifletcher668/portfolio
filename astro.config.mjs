e757bf0ddb037d05da6bd002c301a6d9ab1dc198
Isiah Fletcher <Isiahfletcher7@gmail.com>
2025-01-19 20:15:53 -0800

    Add 3-js course page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
+import path from "path";
 
 // https://astro.build/config
 export default defineConfig({
